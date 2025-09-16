22f0dc94fc25ca25a8032d1b2ff8e02310cf8bc9
FarelAryaP <farelpramudya10@gmail.com>
2025-09-16 16:35:50 +0700

    small fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
-            // 'student.auth' => \App\Http\Middleware\StudentAuth::class,
+            'user.auth' => \App\Http\Middleware\UserAuth::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
